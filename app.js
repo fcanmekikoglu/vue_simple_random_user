29da1cd1be15af8a621a1c6e6eb8d3727df7c7aa
fcanmekikoglu <fcanmekikoglu@gmail.com>
2022-10-07 16:44:43 +0300

    simple random user with vue

---
+++ app.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            firstName: 'Can',
+            lastName: 'Mekikoğlu',
+            email: 'mail@mail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/8.jpg'
+        }
+    },
+    methods: {
+        async getUser(){
+            const res = await fetch('https://randomuser.me/api')
+            const {results} = await res.json()
+            console.log(results)
+            
+            this.firstName = results[0].name.firstName
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+        }
+    }
+})
+
+app.mount('#app')
